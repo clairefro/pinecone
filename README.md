0f9b0f169c1d9fa4260261dee909a0f436b22a6f
Claire Froelich <claire.froelich@gmail.com>
2021-01-26 20:52:09 -0800

    Add first three rules

---
+++ README.md
@@ -0,0 +1,22 @@
+## pinecone
+
+A very niche-use node tool for converting redwood TUTORIAL (pt1) from .com site format to "learn" site format
+
+### Install
+
+```
+yarn install
+```
+
+### Use
+
+Go one dir ABOVE pinecone so you can run it pretending it's a binary 🙃.
+
+Download the latest TUTORIAL.md to your local from (here)[https://raw.githubusercontent.com/redwoodjs/redwoodjs.com/main/TUTORIAL.md]. This will be the input file.
+
+As a second optional arg allows you to specifcy the output path for the converted file (defaults to `./TUORIAL_LEARN.md`)
+
+```
+cd ..
+node pinecone <input/file/path> [output/file/path]
+```
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+#!/usr/bin/env node
+
 const fs = require("fs");
 const chalk = require("chalk");
 
-const info = (info) => {
+const info = (...info) => {
 	console.log(chalk.green(info));
 };
 
 info("Reading file...\n");
 
 const sourceFilePath = process.argv[2];
+const outputPath = process.argv[3] || "./TUTORIAL_LEARN.md";
 
 if (!sourceFilePath) {
 	throw new Error("Must provide source file path as first arg");
@@ -18,5 +21,40 @@ const sourceText = fs.readFileSync(sourceFilePath, "utf8", function (err, data) 
 	return data.toString();
 });
 
-info("[[ PREVIEW ]]");
-console.log(sourceText.slice(0, 250) + "...");
+info("[[ SOURCE DOC PREVIEW ]]");
+console.log(sourceText.slice(0, 250) + "...\n");
+
+// RULES:
+// 1. close all <img> tags
+// 2. convert tutorial internal links to relative paths (https://redwoodjs.com/tutorial/getting-dynamic#creating-a-post-editor -> ./getting-dynamic#creating-a-post-editor -> )
+// 3. convert line highlight syntax
+// 4. split into sections
+// 5. insert frontmatter
+
+let outputText = sourceText;
+
+// 1. close all <img> tags
+info("Closing image tags...");
+
+const unclosedImgTagRegex = /(<(?:img).+[^/?\s?])(>)/g;
+outputText = outputText.replace(unclosedImgTagRegex, "$1/$2");
+
+// 2. convert tutorial internal links to relative paths (https://redwoodjs.com/tutorial/getting-dynamic#creating-a-post-editor -> ./getting-dynamic#creating-a-post-editor -> )
+info("Converting internal tutorial links to relative paths...");
+
+const originalInternalTutorialLinkRegex = /\]\((https:\/\/redwoodjs.com\/tutorial\/)(.+?)\)/g;
+outputText = outputText.replace(originalInternalTutorialLinkRegex, "./$2");
+
+// 3. convert line highlight syntax (add a space between lang and {})
+info("Fixing up code highlighting syntax...");
+
+const originalCodeHighlightRegex = /(```\w+)(\{[\d,-]+\})/g;
+outputText = outputText.replace(originalCodeHighlightRegex, "$1 $2");
+
+info("Cooking up your doctored file at " + outputPath);
+try {
+	fs.writeFileSync(outputPath, outputText);
+} catch (err) {
+	console.error(err);
+}
+info("Done!");
